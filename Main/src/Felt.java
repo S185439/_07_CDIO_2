56f10560e889757f2011386fdb59e4686d02eee5
Jonathan <jonathan_madsen@hotmail.com>
2022-10-18 21:17:52 +0200

    Felt er lavet

--- Main/src/Felt.java
+++ Main/src/Felt.java
@@ -1,2 +1,27 @@
 public class Felt {
+    String outputTekst;
+    String navn;
+    int transaktionsVaerdi;
+
+    public Felt(String _navn, int _tVaerdi, String _outTekst) {
+        navn = _navn;
+        transaktionsVaerdi = _tVaerdi;
+        outputTekst = _outTekst;
+    }
+
+    public void setOutputTekst(String nyTekst) {
+        this.outputTekst = nyTekst;
+    }
+
+    public String getOutputTekst() {
+        return outputTekst;
+    }
+
+    public void setTransaktionsVaerdi(int nyVaerdi) {
+        this.transaktionsVaerdi = nyVaerdi;
+    }
+
+    public int getTransaktionsVaerdi() {
+        return transaktionsVaerdi;
+    }
 }
