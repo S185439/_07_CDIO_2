896c06b603299075040d565fed4226a2ca6a6bfc
Jonathan <jonathan_madsen@hotmail.com>
2022-10-18 21:21:36 +0200

    Kommentarer givet

--- Main/src/Felt.java
+++ Main/src/Felt.java
@@ -3,24 +3,25 @@ public class Felt {
     String navn;
     int transaktionsVaerdi;
 
+    //Constructor for Felt
     public Felt(String _navn, int _tVaerdi, String _outTekst) {
         navn = _navn;
         transaktionsVaerdi = _tVaerdi;
         outputTekst = _outTekst;
     }
 
+    // Getter og Setter for output-teksten
     public void setOutputTekst(String nyTekst) {
         this.outputTekst = nyTekst;
     }
-
     public String getOutputTekst() {
         return outputTekst;
     }
 
+    //Getter og Setter for transaktionsværdi
     public void setTransaktionsVaerdi(int nyVaerdi) {
         this.transaktionsVaerdi = nyVaerdi;
     }
-
     public int getTransaktionsVaerdi() {
         return transaktionsVaerdi;
     }
