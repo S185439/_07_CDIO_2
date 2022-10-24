5031a8cceed680b42b8289c2fa34a86479a29cc3
Valdemar Ottesen <ottesenvaldemar@gmail.com>
2022-10-24 14:08:32 +0200

    Skrevet kommentarer til alle metoder

--- Main/src/Raflebaeger.java
+++ Main/src/Raflebaeger.java
@@ -3,12 +3,20 @@ public class Raflebaeger {
     Terning terning1;
     Terning terning2;
     int sumAfTerninger;
+
+    /**
+     * Constructor til et RafleBaeger, opretter 2 terninge object, og saetter sumAfTerninger til 0
+     */
     public Raflebaeger() {
         this.terning1 = new Terning();
         this.terning2 = new Terning();
         this.sumAfTerninger = 0;
     }
 
+    /**
+     * Bruger slaaTerning på begge terninger, og saetter sumAfTerninger til summen af de 2 terningers oejenVaerdi
+     * @return Den nye sumAfTerninger efter terninger er slaaet
+     */
     public int slaaAlleTerninger() {
         terning1.slaaTerning();
         terning2.slaaTerning();
@@ -16,6 +24,10 @@ public class Raflebaeger {
         return sumAfTerninger;
     }
 
+    /**
+     * Tjekker om sumAfTerninger er lig 0, giver fejlbesked, da man ikke kan have en sum af 0
+     * @return Hvis ikke sumAfTerninger lig 0, returnere SumAfTerninger
+     */
     public int getSumAfTerninger() {
         if (sumAfTerninger == 0) {
             System.out.println("Terninger er ikke blevet slået, der er ikke nogen sum vaerdi");
