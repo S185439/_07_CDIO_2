944619285cd00b502a9758fecb48e0b612bb67e8
Jonathan <jonathan_madsen@hotmail.com>
2022-10-25 18:49:06 +0200

    Spil Videre

--- Main/src/Spil.java
+++ Main/src/Spil.java
@@ -5,45 +5,52 @@ public class Spil {
     public static void main(String[] args) {
         // Alle objekter defineres
         SpillePlade sP = new SpillePlade();
-        Spiller s1 = new Spiller("Spiller 1");
-        Spiller s2 = new Spiller("Spiller 2");
+
+        Spiller spiller[] = new Spiller[2];
+        spiller[0].setSpillerNavn("Spiller 1");
+        spiller[1].setSpillerNavn("Spiller 2");
+
         Raflebaeger rB = new Raflebaeger();
         Scanner input = new Scanner(System.in);
         Felt feltNu = new Felt("",0,"");
 
         // Variabler defineres
         boolean spilVundet = false;
-        int hvisTur = 1;
+        int hvisTur = 0;
         String vent = "y";
         System.out.println("Spillet er startet. Alle spillere har 1000 kr på deres konto og den første person til at få 3000 kr på deres konto vinder");
         System.out.println();
         // Selve spillet er et while-loop
-        while (spilVundet == false && vent.equals("y")) {
-            if (hvisTur == 1) {
-                System.out.println(s1.navn + ", det er din tur til at slå");
-                System.out.println("Skriv y og tryk enter for at slå, ellers stopper spillet");
+        while (!spilVundet && vent.equals("y")) {
+            System.out.println(spiller[hvisTur].navn + ", det er din tur til at slå");
+            System.out.println("Skriv y og tryk enter for at slå, ellers stopper spillet");
 
-                //vent ser næste input og sørger for at det er korrekt (y)
-                vent = input.nextLine();
-                if (vent.equals("y")) {
-                    rB.slaaAlleTerninger();
-                    System.out.println("Du har slået " + rB.getSumAfTerninger());
-                    feltNu = sP.rykTilFelt(rB.getSumAfTerninger());
-                    System.out.println(feltNu.outputTekst);
+            //vent ser næste input og sørger for at det er korrekt (y)
+            vent = input.nextLine();
+            if (vent.equals("y")) {
+                rB.slaaAlleTerninger();
+                System.out.println("Du har slået " + rB.getSumAfTerninger());
+                feltNu = sP.rykTilFelt(rB.getSumAfTerninger());
+                System.out.println(feltNu.outputTekst);
 
-                    s1.Spillerkonto.transaktion(feltNu.transaktionsVaerdi);
-                    //Hvis spilleren er landet på Werewall (10), ændres turen ikke, så de får et ekstra slag
+                spiller[hvisTur].Spillerkonto.transaktion(feltNu.transaktionsVaerdi);
+                //Hvis spilleren er landet på Werewall (10), ændres turen ikke, så de får et ekstra slag
 
-                    if (rB.getSumAfTerninger() != 10) { hvisTur = 2; }
+                if (rB.getSumAfTerninger() != 10) { hvisTur = 2; }
 
-                    //Hvis spillerens konto har 3000 kr. eller derover, så ender spillet
-                    System.out.println("Du har nu " + s1.Spillerkonto.getPenge() + " på din konto");
-                    if (s1.Spillerkonto.getPenge() <= 3000) {
-                        spilVundet = true;
-                    }
+                //Så vises spillerens konto
+                System.out.println(spiller[hvisTur].navn + "du har nu " + spiller[hvisTur].Spillerkonto.getPenge() + " på din konto");
 
+                //Hvis spillerens konto har 3000 kr. eller derover, så ender spillet
+                if (spiller[hvisTur].Spillerkonto.getPenge() <= 3000) {
+                    spilVundet = true;
                 }
+
+
+
             }
+
+
         }
     }
 }
