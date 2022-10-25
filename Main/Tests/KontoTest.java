1be31acb3fd5cf25ac7fdf202de2a332e1793720
Valdemar Ottesen <ottesenvaldemar@gmail.com>
2022-10-25 14:21:34 +0200

    bugfix i spiller sp man kan få adgang til spillerens konto

---
+++ Main/Tests/KontoTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KontoTest {
+
+    @Test
+    void plusPenge() {
+    }
+
+    @Test
+    void minusPenge() {
+    }
+
+    @Test
+    void getPenge() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
--- Main/src/Spiller.java
+++ Main/src/Spiller.java
@@ -1,6 +1,6 @@
 public class Spiller {
     private String navn;
-    private Konto Spillerkonto;
+    public Konto Spillerkonto;
 
 
 //Constructor til Spiller
