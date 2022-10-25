ccba1fe8f2e7b57887376d1ceee8d0ef76bdb349
Valdemar Ottesen <ottesenvaldemar@gmail.com>
2022-10-25 14:01:37 +0200

    konto færdig

--- Main/src/Konto.java
+++ Main/src/Konto.java
@@ -1,2 +1,25 @@
 public class Konto {
+
+    private int penge;
+
+    Konto() {
+        int startVaerdi = 1000;
+        penge = startVaerdi;
+    }
+
+    public void plusPenge(int vaerdi) {
+        penge = penge+vaerdi;
+    }
+    public void minusPenge(int vaerdi) {
+        penge = penge-vaerdi;
+    }
+
+    public int getPenge() {
+        return penge;
+    }
+
+    public String toString() {
+        return Integer.toString(penge);
+    }
+
 }
