a72ba8047f4bac554fa0a69aec9db46ba7c8d3e8
sandravalinee <sandyvalinee@gmail.com>
2022-10-25 08:59:26 +0200

    Spiller

--- Main/src/Spiller.java
+++ Main/src/Spiller.java
@@ -1,2 +1,20 @@
 public class Spiller {
+    private String navn;
+    private Konto Spillerkonto;
+
+
+//Constructor til Spiller
+public Spiller (String navn) {
+    this.navn = navn;
+    Konto = new SpillerKonto();
 }
+
+// Getter og Setter for spillerens navn
+public void setSpillerNavn(String navn){
+    this.navn = navn;
+}
+public String getSpillerNavn(){
+    return navn;
+}
+}
+
