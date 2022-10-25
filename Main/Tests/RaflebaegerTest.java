c448cd2ae6813c154fd77f6f27c156ad9c5e1190
Valdemar Ottesen <ottesenvaldemar@gmail.com>
2022-10-25 13:33:32 +0200

    test af terninger og test af raflebaeger faerdig

--- Main/Tests/RaflebaegerTest.java
+++ Main/Tests/RaflebaegerTest.java
@@ -1,14 +1,19 @@
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-class TerningStub extends Terning {
-
-    @Override
-    public int slaaTerning() {
-        return super.slaaTerning();
-    }
-}
-
 class RaflebaegerTest {
+        Raflebaeger raflebaeger = new Raflebaeger();
+        @Test
+        void slaaAlleTerninger() {
+            for (int t = 0; t < 10; t++) {
+                int tempSum = raflebaeger.slaaAlleTerninger();
+                assertTrue(tempSum >= 1 && tempSum <= 12);
+            }
+        }
 
+        @Test
+        void getSumAfTerninger() {
+            int tempSum = raflebaeger.slaaAlleTerninger();
+            assertEquals(tempSum, raflebaeger.getSumAfTerninger());
+        }
 }
