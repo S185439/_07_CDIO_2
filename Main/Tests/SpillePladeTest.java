6813f1b160862ce9f597c2cc8df660014fed3038
Jonathan <jonathan_madsen@hotmail.com>
2022-10-25 14:36:28 +0200

    SpillePlade Test

--- Main/Tests/SpillePladeTest.java
+++ Main/Tests/SpillePladeTest.java
@@ -6,10 +6,12 @@ class SpillePladeTest {
 
     SpillePlade sP = new SpillePlade();
 
+
     @Test
     void rykTilFelt() {
-
         Felt f1 = sP.rykTilFelt(2);
         assertEquals("Tower", f1.navn);
+        f1 = sP.rykTilFelt(7);
+        assertEquals(0, f1.transaktionsVaerdi);
     }
 }
